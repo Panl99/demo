8aec6d099babfed91120fe9c52705663b1a46ce5
outman <panl99@126.com>
2020-09-17 21:57:49 +0800

    单例模式

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
 - [二叉树的层次遍历](https://github.com/Panl99/demo/blob/master/src/main/java/com/outman/demo/leetcode/BinaryTreeLevelOrderTraversal.java)
 - [前K个高频元素](https://github.com/Panl99/demo/blob/master/src/main/java/com/outman/demo/leetcode/TopkFrequentElements.java)
 - [翻转二叉树](https://github.com/Panl99/demo/blob/master/src/main/java/com/outman/demo/leetcode/InvertBinaryTree.java)
+
+
+- [单例模式](https://github.com/Panl99/demo/blob/master/src/main/java/com/outman/demo/leetcode/Singleton.java)
+
+
+## 剑指Offer
+- 基础扎实全面：编程语言、数据结构、算法等
+- 高质量代码：正确、完整、鲁棒（考虑边界条件、特殊输入(null、空串)、错误处理）
+- 思路清晰：分析、解决复杂问题
+- 优化效率：能从时间、空间复杂度两方面优化算法
+- 具备优秀的沟通、学习、发散思维能力等
+
+
---
+++ src/main/java/com/outman/demo/leetcode/Singleton.java
@@ -0,0 +1,64 @@
+package com.outman.demo.leetcode;
+
+/**
+ * @create 2020/9/17 21:27
+ * @auther outman
+ **/
+public class Singleton {
+    /**
+     * 静态内部类模式
+     * 线程安全、效率高、可延时加载
+     * ** 推荐 **
+     */
+    public static class Singleton3 {
+        private Singleton3() {}
+        private static class SingletonInstance {
+            private static final Singleton3 INSTANCE = new Singleton3();
+        }
+        public static Singleton3 getInstance() {
+            return SingletonInstance.INSTANCE;
+        }
+    }
+
+    /**
+     * 枚举
+     * 枚举元素本身就是单例
+     * 线程安全、效率高、不能延时加载、可以防反射 反序列化调用
+     */
+    public enum Singleton4 {
+        INSTANCE;
+
+        public void singletonOperation() {
+            //do something
+        }
+    }
+
+    /**
+     * 懒汉模式
+     * 只适用于单线程
+     */
+    public static class Singleton1 {
+        private Singleton1() {}
+        private static Singleton1 instance = null;
+        public static Singleton1 getInstance() {
+            if (instance == null) {
+                instance = new Singleton1();
+            }
+            return instance;
+        }
+    }
+
+    /**
+     * 饿汉模式
+     * 不能延迟加载
+     * 初始化时就加载好实例，要是到头来不用，浪费
+     */
+    public static class Singleton2 {
+        private Singleton2() {}
+        private static Singleton2 instance = new Singleton2();
+        public static Singleton2 getInstance() {
+            return instance;
+        }
+    }
+
+}
