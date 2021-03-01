858a3fdae8f43f9aa9f874f20915c182bcd9a9d3
lp <livpan@qq.com>
2021-03-01 22:49:35 +0800

    代理模式

---
+++ demo-common/src/main/java/com/outman/democommon/designpatterns/proxypattern/Company.java
@@ -0,0 +1,10 @@
+package com.outman.democommon.designpatterns.proxypattern;
+
+/**
+ * @create 2021/3/1 22:35
+ * @auther outman
+ * @description
+ **/
+public interface Company {
+    void findWorker(String title);
+}
---
+++ demo-common/src/main/java/com/outman/democommon/designpatterns/proxypattern/Main.java
@@ -0,0 +1,13 @@
+package com.outman.democommon.designpatterns.proxypattern;
+
+/**
+ * @create 2021/3/1 22:48
+ * @auther outman
+ * @description
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Company company = new Proxy();
+        company.findWorker("Java");
+    }
+}
