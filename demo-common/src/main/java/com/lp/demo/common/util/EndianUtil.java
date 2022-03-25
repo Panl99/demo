ed38b96a00ff221b79ce8c7a8930fe5f5c987686
lp <livpan@qq.com>
2022-03-25 17:17:51 +0800

    大小端转换

--- demo-common/src/main/java/com/lp/demo/common/util/EndianUtil.java
+++ demo-common/src/main/java/com/lp/demo/common/util/EndianUtil.java
@@ -39,12 +39,16 @@ public class EndianUtil {
 
 
         String heartRateLittleEndian = "28083b623c000000";
-        int heartRateLittleEndianTime = 0x28083b62;
-        int heartRateLittleEndianData = 0x3c000000;
-        int heartRateBigEndianTime = 0x623b0828;
-        int heartRateBigEndianData = 0x0000003c;
+//        int heartRateLittleEndianTime = 0x28083b62;
+//        int heartRateLittleEndianData = 0x3c000000;
+//        int heartRateBigEndianTime = 0x623b0828;
+//        int heartRateBigEndianData = 0x0000003c;
+        String heartRateLittleEndianTime = "28083b62";
+        String heartRateLittleEndianData = "3c000000";
+        String heartRateBigEndianTime = "623b0828";
+        String heartRateBigEndianData = "0000003c";
 
-        StringBuilder big2Little = new StringBuilder();
+/*        StringBuilder big2Little = new StringBuilder();
         big2Little.append(convertEndian(heartRateBigEndianTime, 4));
         big2Little.append(convertEndian(heartRateBigEndianData, 4));
         System.out.println("big -> little result = " + big2Little);
@@ -57,21 +61,38 @@ public class EndianUtil {
         little2Big.append(convertEndian(heartRateLittleEndianData, 4));
         System.out.println("little -> big result = " + little2Big);
 
-        System.out.println(">>>>>>>>>>" + Integer.reverseBytes(heartRateLittleEndianTime));
+        System.out.println(">>>>>>>>>>" + Integer.reverseBytes(Integer.parseInt(heartRateLittleEndianTime, 16)));*/
+
+        String oneBytes = "62";
+        String twoBytes = "623b";
+        String threeBytes = "623b08";
+        String fourBytes = "623b0828";
+        String fiveBytes = "623b082801";
+
+//        System.out.println("convertEndian(oneBytes, 1) = " + convertEndian(oneBytes, 1)); // 1个字节的不用转
+        System.out.println("convertEndian(twoBytes, 2) = " + convertEndian(twoBytes, 2));
+//        System.out.println("convertEndian(threeBytes, 3) = " + convertEndian(threeBytes, 4));
+        System.out.println("convertEndian(fourBytes, 4) = " + convertEndian(fourBytes, 4));
+//        System.out.println("convertEndian(fiveBytes, 5) = " + convertEndian(fiveBytes, 10)); // parse会超出int范围
     }
 
     /**
      * 大小端转换
      *
-     * @param value    大端/小端数据（16进制最大4字节int型数据，如：0x623b0828）
+     * @param value    大端/小端数据（16进制最大4字节）
      * @param capacity 字节数组容量（取决于数据字节数）
      * @return
      */
-    public static String convertEndian(int value, int capacity) {
+    public static String convertEndian(String value, int capacity) {
         System.out.println("value = " + value);
         ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[capacity]);
         byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
-        byteBuffer.asIntBuffer().put(value);
+        if (capacity % 4 == 0) {
+            byteBuffer.asIntBuffer().put(Integer.parseInt(value, 16));
+        } else if (capacity % 2 == 0) {
+            byteBuffer.asShortBuffer().put(Short.parseShort(value, 16));
+        }
+
         StringBuilder s = new StringBuilder();
         for (byte b : byteBuffer.array()) {
             String hexString = Integer.toHexString(b);
@@ -118,4 +139,32 @@ public class EndianUtil {
 //        System.out.println("little -> big = " + s);
         return s.toString();
     }*/
+
+    private byte[] shortToByteArray(short value, boolean bigEndian) {
+        byte[] bytes;
+        byte b1 = (byte) ((value >> 24) & 0xFF);
+        byte b0 = (byte) (value & 0xFF);
+        if (bigEndian) {
+            bytes = new byte[]{b1, b0};
+        } else {
+            bytes = new byte[]{b0, b1};
+        }
+
+        return bytes;
+    }
+
+    private byte[] intToByteArray(int value, boolean bigEndian) {
+        byte[] bytes;
+        byte b3 = (byte) ((value >> 24) & 0xFF);
+        byte b2 = (byte) ((value >> 16) & 0xFF);
+        byte b1 = (byte) ((value >> 8) & 0xFF);
+        byte b0 = (byte) (value & 0xFF);
+
+        if (bigEndian) {
+            bytes = new byte[]{b3, b2, b1, b0};
+        } else {
+            bytes = new byte[]{b0, b1, b2, b3};
+        }
+        return bytes;
+    }
 }
