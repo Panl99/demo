ac8af923696927e737871682e5efa5d3868701eb
lp <livpan@qq.com>
2022-09-04 17:19:24 +0800

    ZipUtilDemo

---
+++ demo-common/src/main/java/com/lp/demo/common/util/hutool/ZipUtilDemo.java
@@ -0,0 +1,203 @@
+package com.lp.demo.common.util.hutool;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.ZipUtil;
+import com.lp.demo.common.exception.DisplayableException;
+import com.lp.demo.common.service.ThreadLocalService;
+import com.lp.demo.common.util.StringUtil;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author lp
+ * @date 2022/9/3 16:31
+ * @desc 解压、读取压缩包下内容
+ **/
+public class ZipUtilDemo {
+
+    /**
+     * 背景：
+     * 上传智能手表表盘，手动一个一个上传太慢，按压缩包格式批量解析上传
+     * 表盘信息：
+     *      文件1：xxx.bin表盘文件
+     *      文件2：.png/.jpg等格式的1张表盘图片
+     *      文件3：xxx.txt表盘信息描述文件，内容包含：
+     *          名称：xxx
+     *          风格：xxx
+     *          标签：aa;bb;cc
+     *          作者：xxx
+     *          描述：xxx
+     *          价格：xxx
+     * <p>
+     * 压缩包格式：xxx.zip
+     * 压缩文件编码：utf-8
+     */
+    public static void main(String[] args) {
+        // 校验压缩包、压缩包下文件格式
+
+        // 校验压缩包下文件内容格式
+
+        // 解压文件
+
+        // 读取文件内容 并处理
+
+        // 删除解压文件
+
+        String path = "D:\\Code\\jingxun\\test333.zip";
+        String pathCn = "D:\\Life\\work\\jingxun\\智能手表\\表盘文件-test.zip";
+        File file = new File(path);
+//        unzip(file); Charset.defaultCharset();
+        unzip(pathCn);
+    }
+
+    public static void preCheck(File file) {
+        File[] files = file.listFiles();
+        if (files == null) {
+            System.out.println("files is null");
+            throw new DisplayableException(2001, "files is null!");
+        }
+        System.out.println(">>>>>> " + file.getName());
+
+        if (files.length < 1) {
+            throw new DisplayableException(2001, "files length < 1!");
+        }
+    }
+
+    private static final String[] imgFileType = {"png", "jpg", "jpeg", "bmp"};
+    public static void read(File file, List<Map<String, String>> contentList) {
+        File[] files = file.listFiles();
+        if (files == null) {
+            System.out.println("files is null");
+            return;
+        }
+
+        System.out.println(">>>>>> " + file.getName());
+
+        Map<String, String> item = new HashMap<>();
+
+        Iterator<File> iterator = Arrays.asList(files).iterator();
+        while (iterator.hasNext()) {
+            File fileN = iterator.next();
+            if (fileN.isFile()) {
+                if (FileUtil.isEmpty(fileN)) {
+                    continue;
+                }
+
+                String fileName = fileN.getName();
+//                String fileType = fileName.substring(fileName.lastIndexOf(".") + 1);
+                // 读取文件
+                if (fileName.endsWith(".bin")) {
+                    byte[] binBytes = IoUtil.readBytes(FileUtil.getInputStream(fileN));
+                    item.put("binBytes", String.valueOf(binBytes.length));
+                }
+
+//                if (!ArrayUtils.contains(imgFileType, fileType.toLowerCase())) {
+//                    System.out.println("Image format error! only supports [png,jpg,jpeg,bmp] format image, image = "+ fileName);
+//                    return;
+//                }
+                if (fileName.endsWith(".png")) {
+                    byte[] imgBytes = IoUtil.readBytes(FileUtil.getInputStream(fileN));
+                    item.put("imgBytes", String.valueOf(imgBytes.length));
+                }
+                if (fileName.endsWith(".txt")) {
+                    if (FileUtil.isEmpty(fileN)) {
+                        System.err.println("txt is empty!");
+                        return;
+                    }
+
+                    // 读取文件信息
+                    List<String> lines = FileUtil.readLines(fileN, CharsetUtil.defaultCharset());
+                    for (String line : lines) {
+                        String[] content = line.trim().split("：");
+                        switch (content[0]) {
+                            case "名称":
+                                item.put("name", content[1]);
+                                break;
+                            case "风格":
+                                item.put("style", content[1]);
+                                break;
+                            case "标签":
+                                item.put("tag", content[1]);
+                                break;
+                            case "作者":
+                                item.put("author", content[1]);
+                                break;
+                            case "描述":
+                                item.put("desc", content[1]);
+                                break;
+                            case "价格":
+                                item.put("price", content[1]);
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+
+            } else if (fileN.isDirectory()) {
+                if (FileUtil.isDirEmpty(fileN)) {
+                    continue;
+                }
+                read(fileN, contentList);
+            }
+        }
+
+        if (!item.isEmpty()) {
+            contentList.add(item);
+        }
+    }
+
+    public static List read(File file) {
+        List<Map<String, String>> content = new ArrayList<>();
+        if (file.isDirectory()) {
+            read(file, content);
+        } else {
+            System.out.println("unzip file is not dir!");
+        }
+
+        System.out.println("contentList = " + content);
+        return content;
+    }
+
+    public static List unzip(String path) {
+        File unzipFile = ZipUtil.unzip(path, CharsetUtil.systemCharset());
+        String absolutePath = unzipFile.getAbsolutePath();
+        System.out.println("absolutePath = " + absolutePath);
+
+        return read(unzipFile);
+    }
+
+    public static List unzip(InputStream in) throws IOException {
+        String fileName = (String) ThreadLocalService.getInstance().getAndRemove();
+        if (StringUtil.isBlank(fileName)) {
+            fileName = "dialUploadTemp";
+        }
+        File destDir = FileUtil.mkdir(System.getProperty("java.io.tmpdir") + File.separator + fileName);
+        File file = ZipUtil.unzip(in, destDir, CharsetUtil.systemCharset());
+        List content = read(file);
+        FileUtil.del(destDir);
+        return content;
+    }
+
+    public static void unzip(byte[] bytes) throws IOException {
+        unzip(new ByteArrayInputStream(bytes));
+    }
+
+    public static void unzip(MultipartFile file) throws IOException {
+        unzip(file.getInputStream());
+    }
+
+}
--- demo-main/src/main/java/com/lp/demo/main/controller/MainController.java
+++ demo-main/src/main/java/com/lp/demo/main/controller/MainController.java
@@ -1,9 +1,16 @@
 package com.lp.demo.main.controller;
 
+import com.lp.demo.common.service.ThreadLocalService;
+import com.lp.demo.common.util.hutool.ZipUtilDemo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @create 2020/12/26 23:32
@@ -24,5 +31,11 @@ public class MainController {
     public String testGet() {
         log.info("start testGet");
         return "Hello testGet";
+    }
+
+    @PostMapping("/upload")
+    public List uploadTest(MultipartFile file) throws IOException {
+        ThreadLocalService.getInstance().setValue(file.getOriginalFilename());
+        return ZipUtilDemo.unzip(file.getInputStream());
     }
 }
