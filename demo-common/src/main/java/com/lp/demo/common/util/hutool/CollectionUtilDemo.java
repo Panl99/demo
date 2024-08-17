ced842347c0ada8cc20d541590b3232d4763d138
lp <livpan@qq.com>
2024-08-17 16:48:06 +0800

    对嵌套列表分组(flatMapping)

--- demo-common/src/main/java/com/lp/demo/common/util/hutool/CollectionUtilDemo.java
+++ demo-common/src/main/java/com/lp/demo/common/util/hutool/CollectionUtilDemo.java
@@ -2,8 +2,12 @@ package com.lp.demo.common.util.hutool;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.alibaba.fastjson.JSONObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.collections4.ListUtils;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,6 +19,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -56,20 +63,48 @@ public class CollectionUtilDemo {
         }
     };
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class A {
+        Integer a;
+        String aName;
+        List<B> bList;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class B {
+        Integer b;
+        String bName;
+        List<C> cList;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class C {
+        Integer c;
+        String cName;
+    }
+
 
     static class ListDemo {
 
         public static void main(String[] args) {
-            System.out.println(" ----------------------------List test1-------------------------- ");
+            System.out.println(" ----------List test1: 列表操作：差集、交集、并集等---------- ");
             test1();
-            System.out.println(" ----------------------------List test2-------------------------- ");
+            System.out.println(" ----------List test2: 列表合并---------- ");
             test2();
-            System.out.println(" ----------------------------List test3-------------------------- ");
+            System.out.println(" ----------List test3: 列表分成均等n份（注重每份几个，前n-1份每份数量都相同，最后一份为剩余数量。如总数10，每份3个，会分成4组结果为：[3,3,3,1]）---------- ");
             test3();
-            System.out.println(" ----------------------------List test4-------------------------- ");
+            System.out.println(" ----------List test4: 列表分成n份，非均等（注重分成几份，总共要分成n份，均等为每份分完后，剩余的再分给前m个。如总数10，分成3组，分配结果为：[4,3,3]）---------- ");
             test4();
-            System.out.println(" ----------------------------List test5-------------------------- ");
+            System.out.println(" ----------List test5: 两个嵌套列表比较同一级子列表中元素是否存在相同项，存在则交换使得同级子列表不能存在相同项---------- ");
             test5();
+            System.out.println(" ----------List test6: 嵌套列表分组---------- ");
+            test6();
 
         }
 
@@ -312,7 +347,200 @@ public class CollectionUtilDemo {
                 }
             }
 
+        /**
+         * 嵌套列表分组
+         */
+        public static void test6() {
 
+            List<A> aList = Arrays.asList(
+                    new A(1, "A1",
+                            Arrays.asList(new B(11, "B11",
+                                            Arrays.asList(new C(111, "C111"), new C(112, "C112"))),
+                                    new B(12, "B12",
+                                            Collections.singletonList(new C(121, "C121"))))),
+                    new A(2, "A2",
+                            Arrays.asList(new B(21, "B21",
+                                            Collections.singletonList(new C(211, "C211"))),
+                                    new B(22, "B22",
+                                            Arrays.asList(new C(221, "C221"), new C(222, "C222")))))
+            );
+//            System.out.println(JSONObject.parseArray(JSONObject.toJSONString(aList)));
+            /**
+             * 下列结构 按A的a属性分组->再按A的子列表B的b属性分组，分组后得到该结构的数据：Map<Integer(a), Map<Integer(b), List<C>>>
+             * [
+             *    {
+             * 		"a": 1,
+             * 		"aName": "A1",
+             * 		"bList": [
+             *            {
+             * 				"b": 11,
+             * 				"bName": "B11",
+             * 				"cList": [
+             *                    {
+             * 						"c": 111,
+             * 						"cName": "C111"
+             *                    },
+             *                    {
+             * 						"c": 112,
+             * 						"cName": "C112"
+             *                    }
+             * 				]
+             *            },
+             *            {
+             * 				"b": 12,
+             * 				"bName": "B12",
+             * 				"cList": [
+             *                    {
+             * 						"c": 121,
+             * 						"cName": "C121"
+             *                    }
+             * 				]
+             *            }
+             * 		]
+             *    },
+             *    {
+             * 		"a": 2,
+             * 		"aName": "A2",
+             * 		"bList": [
+             *            {
+             * 				"b": 21,
+             * 				"bName": "B21",
+             * 				"cList": [
+             *                    {
+             * 						"c": 211,
+             * 						"cName": "C211"
+             *                    }
+             * 				]
+             *            },
+             *            {
+             * 				"b": 22,
+             * 				"bName": "B22",
+             * 				"cList": [
+             *                    {
+             * 						"c": 221,
+             * 						"cName": "C221"
+             *                    },
+             *                    {
+             * 						"c": 222,
+             * 						"cName": "C222"
+             *                    }
+             * 				]
+             *            }
+             * 		]
+             *    }
+             * ]
+             */
+
+            // 方式1：使用Java9+ 实现：按a分组，并对子列表List<B>按b分组，再得到List<C>
+//            Map<Integer, Map<Integer, List<C>>> result = aList.stream()
+//                    .collect(Collectors.groupingBy(
+//                            A::getA, // 按A的a属性分组
+//                            Collectors.flatMapping(
+//                                    a -> a.getBList().stream(), // 提取List<B>
+//                                    Collectors.groupingBy(
+//                                            B::getB, // 按B的b属性分组
+//                                            Collectors.flatMapping(
+//                                                    b -> b.getCList().stream(), // 提取List<C>
+//                                                    Collectors.toList() // 收集成List<C>
+//                                            )
+//                                    )
+//                            )
+//                    ));
+
+            // 方式2：使用Java8 + 自定义flatMapping实现：
+            Map<Integer, Map<Integer, List<C>>> result = aList.stream()
+                    //.sorted(Comparator.comparing(A::getA))
+                    .collect(Collectors.groupingBy(
+                            A::getA, // 按A的a属性分组
+                            CollectionUtilDemo.flatMapping(
+                                    a -> a.getBList().stream(),//.sorted(Comparator.comparing(B::getB)), // 提取List<B>
+                                    Collectors.groupingBy(
+                                            B::getB, // 按B的b属性分组
+                                            CollectionUtilDemo.flatMapping(
+                                                    b -> b.getCList().stream(),//.sorted(Comparator.comparing(C::getC)), // 提取List<C>
+                                                    Collectors.toList() // 收集成List<C>
+                                            )
+                                    )
+                            )
+                    ));
+            System.out.println("result2: " + JSONObject.parseObject(JSONObject.toJSONString(result)));
+
+            // 方式3：使用Java8 实现：
+            Map<Integer, Map<Integer, List<C>>> resultByJava8 = aList.stream()
+                    .flatMap(a -> a.getBList().stream()
+                            .flatMap(b -> b.getCList().stream()
+                                    .map(c -> new AbstractMap.SimpleImmutableEntry<>(b.getB(), c)))
+                            .map(b -> new AbstractMap.SimpleImmutableEntry<>(a.getA(), b)))
+                    .collect(Collectors.groupingBy(AbstractMap.SimpleImmutableEntry::getKey,
+                            Collectors.mapping(Map.Entry::getValue,
+                                    Collectors.groupingBy(AbstractMap.SimpleImmutableEntry::getKey,
+                                            Collectors.mapping(Map.Entry::getValue,
+                                                    Collectors.toList())))));
+
+            System.out.println("result3: " + JSONObject.parseObject(JSONObject.toJSONString(resultByJava8)));
+
+            /**
+             * {
+             * 	"1": {
+             * 		"11": [
+             *            {
+             * 				"c": 111,
+             * 				"cName": "C111"
+             *            },
+             *            {
+             * 				"c": 112,
+             * 				"cName": "C112"
+             *            }
+             * 		],
+             * 		"12": [
+             *            {
+             * 				"c": 121,
+             * 				"cName": "C121"
+             *            }
+             * 		]
+             * 	},
+             * 	"2": {
+             * 		"22": [
+             *            {
+             * 				"c": 221,
+             * 				"cName": "C221"
+             *            },
+             *            {
+             * 				"c": 222,
+             * 				"cName": "C222"
+             *            }
+             * 		],
+             * 		"21": [
+             *            {
+             * 				"c": 211,
+             * 				"cName": "C211"
+             *            }
+             * 		]
+             * 	}
+             * }
+             */
+        }
+
+
+    }
+
+    // 因不能使用Java9，但想使用flatMapping操作来自定义一个flatMapping。
+    static <T, U, A, R> Collector<T, ?, R> flatMapping(
+            Function<? super T, ? extends Stream<? extends U>> mapper,
+            Collector<? super U, A, R> downstream) {
+
+        BiConsumer<A, ? super U> acc = downstream.accumulator();
+        return Collector.of(downstream.supplier(),
+                (a, t) -> {
+                    try (Stream<? extends U> s = mapper.apply(t)) {
+                        if (s != null) {
+                            s.forEachOrdered(u -> acc.accept(a, u));
+                        }
+                    }
+                },
+                downstream.combiner(), downstream.finisher(),
+                downstream.characteristics().toArray(new Collector.Characteristics[0])
+        );
     }
 
 
@@ -323,20 +551,20 @@ public class CollectionUtilDemo {
 
     static class MapDemo{
         public static void main(String[] args) {
-            System.out.println(" ----------------------------Map test1-------------------------- ");
+            System.out.println(" ----------Map test1: Map<Long, List<?>>按key过滤后，合并value列表，返回：List<Integer>---------- ");
             test1();
-            System.out.println(" ----------------------------Map test2-------------------------- ");
+            System.out.println(" ----------Map test2: Map<Long, List<?>>按value中某项再分组，返回：Map<Object, Map<Object, List<Object>>>---------- ");
             test2();
-            System.out.println(" ----------------------------Map test3-------------------------- ");
+            System.out.println(" ----------Map test3: Map<Long, List<?>>按值条件再分组，返回：Map<Integer, Map<String, List<String>>>---------- ");
             test3();
-            System.out.println(" ----------------------------Map test4-------------------------- ");
+            System.out.println(" ----------Map test4: Map<Integer, Long> 转为 Map<Integer, Double>, 对value计算比值---------- ");
             test4();
-            System.out.println(" ----------------------------Map test5-------------------------- ");
+            System.out.println(" ----------Map test5: Map<String, Map<Integer, Double>> 遍历最外层的Map，并对内层Map中相同键值的Integer项求平均值，然后将结果转换为一个新的Map。 返回：Map<Integer, Double>---------- ");
             test5();
-            System.out.println(" ----------------------------Map test6-------------------------- ");
+            System.out.println(" ----------Map test6: Map<Long, Map<String, Map<Integer, Double>>> 对每个Map的内层 Map<Integer, Double> 的 Double 值求和，并根据求和结果进行排序，求和值相同，排序为平等序号。返回：Map<Long, Integer>---------- ");
             test6();
 
-            System.out.println(" ----------------------------Map test8-------------------------- ");
+            System.out.println(" ----------Map test8: Map<String, Map<Integer, Double>> 1、所有Integer的平均值 返回：Map<Integer, Double>。2、按条件重新分组后，分组后分组的Integer的平均值，返回：Map<String, Map<Integer, Double>>---------- ");
             test8();
 
 
