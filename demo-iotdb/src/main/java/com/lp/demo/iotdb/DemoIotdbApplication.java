e6e2fb819165608eb714de5fd71a7adf8d15728a
lp <livpan@qq.com>
2025-06-14 17:48:54 +0800

    demo-iotdb

---
+++ demo-iotdb/src/main/java/com/lp/demo/iotdb/DemoIotdbApplication.java
@@ -0,0 +1,13 @@
+package com.lp.demo.iotdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIotdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoIotdbApplication.class, args);
+    }
+
+}
---
+++ demo-iotdb/src/test/java/com/lp/demo/iotdb/DemoIotdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.demo.iotdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIotdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
